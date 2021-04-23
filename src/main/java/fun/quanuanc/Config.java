35b7bf2d83c671dae1e97381c25534709b5c6c39
Quanuanc <quanuanc@outlook.com>
2021-04-23 08:19:30 +0800

    fix: add url in Config

--- src/main/java/fun/quanuanc/Config.java
+++ src/main/java/fun/quanuanc/Config.java
@@ -4,6 +4,6 @@ public class Config {
     public static final long SCHEDULE_TASK_PERIOD = 30 * 60 * 1000L;
     public static final String BOT_USERNAME = "";
     public static final String BOT_TOKEN = "";
-    public static final String V2EX_URL = "";
-    public static final String V2EX_URL_FILE = ""; //for test in local
+    public static final String V2EX_URL = "https://www.v2ex.com/feed/all4all.xml";
+    public static final String V2EX_URL_FILE = "/Users/quanuanc/Downloads/all4all.xml"; //本地测试用
 }
